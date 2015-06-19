2ad74b2544389adfd4db3cc3757ca897b9830aba
Wim <github@wikke.net>
2015-06-19 10:49:50 +0200

    messages - email
    log emails in database

--- messages/email.php
+++ messages/email.php
@@ -1,5 +1,8 @@
 <?php
 $debug = 0;
+if(isset($_GET['debug']) && $_GET['debug'] == 1){
+	$debug = 1;
+}
 //require 'connection.php';
 require 'functions.php';
 require '../_core/appinit.php';
@@ -41,14 +44,92 @@ for($i=$total-1;$i>=0;$i--) {
 	
 	if($debug == 1) print_r($email);
 	
+	$email_info = print_r($email,true);
+	
 	$fromaddress = '- not set -';
 	$subject = '- not set -';
+	$date = '- not set -';
+	$message_id = '- not set -';
+	$toaddress = '- not set -';
 	
 	if(isset($email['header']->fromaddress)){
 		$fromaddress = $email['header']->fromaddress;
 	}
 	if(isset($email['header']->subject)){
 		$subject = $email['header']->subject;
+	}
+	if(isset($email['header']->date)){
+		$date = $email['header']->date;
+	}
+	if(isset($email['header']->message_id)){
+		$message_id = $email['header']->message_id;
+	}
+	if(isset($email['header']->toaddress)){
+		$toaddress = $email['header']->toaddress;
+	}
+	
+	/*
+		[body]
+	   [structure] => stdClass Object
+                (
+                    [type] => 0
+                    [encoding] => 0
+                    [ifsubtype] => 1
+                    [subtype] => HTML
+                    [ifdescription] => 0
+                    [ifid] => 0
+                    [lines] => 652
+                    [bytes] => 53821
+                    [ifdisposition] => 0
+                    [ifdparameters] => 0
+                    [ifparameters] => 1
+                    [parameters] => Array
+                        (
+                            [0] => stdClass Object
+                                (
+                                    [attribute] => CHARSET
+                                    [value] => utf-8
+                                )
+
+                        )
+
+                )
+	*/
+	
+	$qry_check = mysql_query("
+		select * from t_email
+		where
+			ifnull(fromaddress,'- not set -') = '" . mysql_real_escape_string($fromaddress) . "'
+			and ifnull(subject,'- not set -') = '" . mysql_real_escape_string($subject) . "'
+			and ifnull(date,'- not set -') = '" . mysql_real_escape_string($date) . "'
+			and ifnull(message_id,'- not set -') = '" . mysql_real_escape_string($message_id) . "'
+			and ifnull(toaddress,'- not set -') = '" . mysql_real_escape_string($toaddress) . "'
+		");
+	
+	if(mysql_num_rows($qry_check) == 0){
+		mysql_query("
+			insert into t_email
+			(
+				raw,
+				fromaddress,
+				subject,
+				date,
+				message_id,
+				toaddress,
+				body
+			)
+			values
+			(
+				raw,
+				fromaddress,
+				subject,
+				date,
+				message_id,
+				toaddress,
+				body
+			)
+			");
+		$id_email = mysql_insert_id($conn);
 	}
 	
 	$qry = mysql_query("
