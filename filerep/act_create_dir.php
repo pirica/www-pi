3c1aeb1da7357976ec13993e3ca77676b0bb146f
Wim <github@wikke.net>
2015-08-05 13:53:18 +0200

    filerep - create dir: fixes

--- filerep/act_create_dir.php
+++ filerep/act_create_dir.php
@@ -2,18 +2,26 @@
 
 $newdir = '';
 
+$server_directory = '';
+
+while($stat = mysql_fetch_array($qry_share_stats)){
+	if($stat['id_share'] == $id_share){
+		$server_directory = $stat['server_directory'];
+	}
+}
+
 if(isset($_POST['newdir']) && $_POST['newdir'] != ''){
 	
 	$newdir = $_POST['newdir'];
 	
 	$is_dir = true;
 	try {
-		$is_dir = is_dir($newdir);
+		$is_dir = is_dir($server_directory . $dir . $newdir);
 	}
 	catch(Exception $e){}
 	
 	if($is_dir !== true){
-		mkdir($dir . $newdir);
+		mkdir($server_directory . $dir . $newdir);
 	}
 	
 }
