538bd1d2699cba1fcb58abe347a78c5b2568abc1
Wim <github@wikke.net>
2015-08-05 14:18:30 +0200

    filerep - create dir: insert in db corrected

--- filerep/act_create_dir.php
+++ filerep/act_create_dir.php
@@ -21,27 +21,25 @@ if($newdir != ''){
 		if( mkdir($server_directory . $dir . $newdir) ){
 		
 			mysql_query("
-				insert into t_file
+				insert into t_directory
 				(
 					id_share,
-					filename ,
+					dirname ,
 					relative_directory,
-					size,
-					version,
+					parent_directory,
 					date_last_modified
 				)
 				values
 				(
 					" . $id_share . ",
-					'" . mysql_real_escape_string($filename) . "',
+					'" . mysql_real_escape_string($newdir) . "',
+					'" . mysql_real_escape_string($dir . $newdir . '/') . "',
 					'" . mysql_real_escape_string($dir) . "',
-					" . $filesize . ",
-					1,
 					'" . date('Y-m-d H:i:s', $modified) . "'
 				)
 				", $conn);
 			
-			goto_action('details', false, 'id_share=' . $id_share . '&dir=' . $dir . $newdir);
+			goto_action('details', false, 'id_share=' . $id_share . '&dir=' . $dir . $newdir . '/');
 		}
 		else {
 			goto_action('create_dir', false, 'id_share=' . $id_share . '&dir=' . $dir . '&newdir=' . $newdir . '&error=notcreated' );
