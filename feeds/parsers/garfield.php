0737b500dcd9f78436b04d396057119453787db2
Wim <github@wikke.net>
2016-04-14 14:15:39 +0200

    rtggsghsh

--- feeds/parsers/garfield.php
+++ feeds/parsers/garfield.php
@@ -1,22 +1,9 @@
 <?php
 
-$str = file_get_contents($feeds['url']);
 
-//$str = file_get_contents('http://garfield.com/');
-//require dirname(__FILE__).'/../../_core/components/phpQuery/phpQuery.php';
-
-
-$doc = phpQuery::newDocumentHTML($str);
-$title = $doc->find('.comic-display img')->attr('src');
-$title = explode('/', $title);
-$title = $title[count($title) - 1];
-$title = explode('.', $title)[0];
-$description = '<img src="' . $doc->find('.comic-display img')->attr('src') . '" alt="">';
+$title = date('Y-m-d', time());
+$description = '<img src="https://s3.amazonaws.com/static.garfield.com/comics/garfield/' . date('Y', time()) . '/' . date('Y-m-d', time()) . '.gif">';
 
 
-/*
-echo $title;
-echo "<br>";
-echo $description
-// */
 ?>
+
