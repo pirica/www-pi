bfdd608f72e76a995d1292201c93b7ec75499cea
Wim <github@wikke.net>
2016-05-04 10:37:03 +0200

    sfghsdfh

--- users/act_set_profileapp.php
+++ users/act_set_profileapp.php
@@ -8,6 +8,29 @@ if(!in_array($field, array('allowed'))){
 	$field = '';
 }
 
+$qry_insert_profile_app = $mysqli->prepare("
+	insert into t_profile_app
+	(
+		id_profile,
+		id_app,
+		allowed
+	)
+	select
+		p.id_profile,
+		a.id_app,
+		0 as allowed
+		
+	from t_profile p
+	cross join t_app a
+	left join t_profile_app pa on pa.id_profile = p.id_profile and pa.id_app = a.id_app
+	where
+		p.active = 1
+		and p.full_access = 0
+		and pa.id_profile_app is null
+		
+	");
+$qry_insert_profile_app->execute();
+	
 $error = 0;
 if($id_app > 0 && $id_profile > 0 && $field != ''){
 	
