939650774db903c85a517ddeffff2d97879b9c77
Wim <github@wikke.net>
2016-05-17 13:57:35 +0200

    core - new top menu

--- users/dsp_topmenu.php
+++ users/dsp_topmenu.php
@@ -1,12 +1,113 @@
 <?php
 
-include 'queries/pr_get_apps.php';
 
 // /*
 if(isset($_GET['newmenu'])) {
+	
+	include 'queries/pr_get_apps_actions.php';
 
 ?>
 
+<div class="topmenu2">
+	<nav class="navbar navbar-default navbar-inverse" role="navigation">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">Wikke.net</a>
+		</div>
+		
+		<?php
+		if(isset($loggedin) && $loggedin === true)
+		{
+			?>
+			<ul class="nav navbar-nav nav-user">
+				<li class="dropdown pull-right">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+						<i class="fa fa-user"></i>
+						<span class="user"><?= $_SESSION['username_safe'] ?></span>
+						<span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu pull-right" role="menu">
+						<li class="disabled"><a href="#">My profile</a></li>
+						<li class="disabled"><a href="#">My stats</a></li>
+						<li class="divider"></li>
+						<li><a href="../users/index.php?action=settings">Settings</a></li>
+						<li><a href="../users/index.php?action=actions">Actions</a></li>
+						<li><a href="../users/index.php?action=profileapps">Profiles - Apps</a></li>
+						<li><a href="../users/index.php?action=profileappactions">Profiles - Actions</a></li>
+						<li class="divider"></li>
+						<li><a href="../users/index.php?action=logout">Log out</a></li>
+					</ul>
+				</li>
+			</ul>
+			<?php
+		}
+		else
+		{
+			?>
+			<ul class="nav navbar-nav nav-user">
+				<li class="dropdown pull-right">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+						<i class="fa fa-user"></i>
+						<span class="user">(Not logged in) </span>
+						<span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu pull-right" role="menu">
+						<li><a href="../users/index.php?action=login">Log in</a></li>
+						<!--<li><a href="../users/index.php?action=register">Register</a></li>-->
+					</ul>
+				</li>
+			</ul>
+			<?php
+		}
+		?>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav nav-apps">
+				<?php
+				while ($menu = mysql_fetch_array($qry_apps)) {
+					$relative_url = $menu['relative_url'];
+					$description = $menu['description'];
+					
+					$class = '';
+					if($menu['is_current'] == 1){
+						$class .= ' active';
+					}
+					
+					if(isset($loggedin) && $loggedin === true){
+						
+					}
+					else if($menu['login_required'] == 1) {
+						$class .= ' disabled';
+						$relative_url = "#";
+					}
+					
+					if($menu['show_in_topmenu'] == 1){
+					?>
+						<li class="<?= $class ?>"><a href="<?= $relative_url ?>"><?= $description ?></a>
+						<li class="<?= $class ?>"><a href="<?= $relative_url ?>" id="btnmenu<?= $menu['id_app'] ?>" data-toggle="collapse" data-target="#submenu<?= $menu['id_app'] ?>" aria-expanded="false"><?= $description ?></a>
+							<ul class="nav collapse" id="submenu<?= $menu['id_app'] ?>" role="menu" aria-labelledby="btnmenu<?= $menu['id_app'] ?>">
+								<li><a href="#">Link 2.1</a></li>
+								<li><a href="#">Link 2.2</a></li>
+								<li><a href="#">Link 2.3</a></li>
+							</ul>
+						</li>
+					<?php
+					}
+				}
+				?>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</nav>
+</div>
+
+<!--
 <div class="topmenu">
 	<nav role="navigation" class="navbar navbar-default navbar-static-top">
 		<div class="container-fluid">
@@ -57,10 +158,24 @@ if(isset($_GET['newmenu'])) {
 					<li><a href="event.html">Event</a></li>
 					
 				</ul>
+				
+				<ul class="nav navbar-nav pull-right">
+					<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+						<i class="fa fa-user"></i>
+						<span class="user">(Not logged in) </span>
+						<span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu pull-right" role="menu">
+						<li><a href="../users/index.php?action=login">Log in</a></li>
+					</ul>
+				</li>
+				</ul>
 			</div>
 		</div>
 	</nav>
 </div>
+-->
 
 <?php
 // */
@@ -68,6 +183,9 @@ if(isset($_GET['newmenu'])) {
 
 // /*
 } else {
+	
+	include 'queries/pr_get_apps.php';
+
 ?>
 
 <div class="topmenu">
---
+++ users/queries/pr_get_apps_actions.php
@@ -0,0 +1,40 @@
+<?php
+
+$request_uri = '';
+if(isset($_SERVER['REQUEST_URI'])){
+	$request_uri = $_SERVER['REQUEST_URI'];
+}
+
+if($request_uri == '' || $request_uri == '/'){
+	$request_uri = '/';
+}
+else {
+	$request_uri = '/' . explode("/", $request_uri)[1];
+}
+//echo '<!--u:'.$request_uri.'-->';
+
+$qry_apps = mysql_query("
+	
+	select
+		a.id_app,
+		a.description,
+		'' as info,
+		a.relative_url,
+		a.show_in_overview,
+		a.show_in_topmenu,
+		a.login_required,
+		
+		case when " . mysql_real_escape_string($request_uri) . " = a.relative_url then 1 else 0 end as is_current
+		
+	from t_app a
+		join t_profile p on p.id_profile = " . $id_profile . "
+		left join t_profile_app pa on pa.id_app = a.id_app and pa.id_profile = p.id_profile
+	where
+		pa.allowed = 1 or p.full_access = 1
+	order by
+		ifnull(a.sort_order, a.id_app)
+		
+	");
+	
+	
+?>
