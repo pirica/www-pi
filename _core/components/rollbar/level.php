1215e039bcfeb1d8b12b333cadef90f10f9bda1d
Wim <github@wikke.net>
2016-10-28 13:35:57 +0200

    rollbar component again

---
+++ _core/components/rollbar/level.php
@@ -0,0 +1,10 @@
+<?php
+
+abstract class Level {
+    const ERROR = 'error';
+    const CRITICAL = 'critical';
+    const WARNING = 'warning';
+    const INFO = 'info';
+    const DEBUG = 'debug';
+}
+?>
