f68eb8f656fe182bf05c19e3b2780657baee2fbc
Wim <github@wikke.net>
2016-11-10 10:22:41 +0100

    comics: mysql -> mysqli

--- comics/connections.php
+++ comics/connections.php
@@ -6,7 +6,6 @@ $mysql_user = 'root';
 $mysql_pw = '';
 require '/etc/mysql/conf.php';
 
-$conn = mysql_connect($mysql_host, $mysql_user, $mysql_pw) ;//or die("Unable to connect to MySQL");
-mysql_select_db($mysql_dbn, $conn) ;//or die("Could not select examples");
+$conn = mysqli_connect($mysql_host, $mysql_user, $mysql_pw, $mysql_dbn) ;//or die("Unable to connect to MySQL");
 
 ?>
