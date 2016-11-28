5924aea518b1aceb90ff117d36b7ace1c909b2ca
Wim <github@wikke.net>
2016-11-28 14:34:58 +0100

    aaaaaa

--- dashboard/index.php
+++ dashboard/index.php
@@ -10,11 +10,11 @@ switch($action->getCode()){
 	case 'weather':
 		$app->jqueryui = true;
 		
-		$app->setHeaderScripts('<link href='http://fonts.googleapis.com/css?family=Nixie+One|Yanone+Kaffeesatz:400,200,700,300' rel='stylesheet' type='text/css'>');
+		$app->setHeaderScripts('<link href="http://fonts.googleapis.com/css?family=Nixie+One|Yanone+Kaffeesatz:400,200,700,300" rel="stylesheet" type="text/css">');
 		$app->setHeaderScripts('<script type="text/javascript" src="scripts/skycons.js"></script>');
 		$app->setHeaderScripts('<script type="text/javascript" src="http://suncalc.net/scripts/suncalc.js"></script>');
-		$app->setHeaderScripts('<link href='styles/weather.css' rel='stylesheet' type='text/css'>');
-		$app->setHeaderScripts('<script src='http://momentjs.com/downloads/moment.min.js'></script>');
+		$app->setHeaderScripts('<link href="styles/weather.css" rel="stylesheet" type="text/css">');
+		$app->setHeaderScripts('<script src="http://momentjs.com/downloads/moment.min.js"></script>');
 		
 		include '../_core/dsp_header.php';
 		include 'dsp_weather.php';
