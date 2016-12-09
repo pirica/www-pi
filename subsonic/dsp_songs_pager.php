934e24d1d53e5e468fc28b90a196d394b92c2d99
Wim <github@wikke.net>
2016-12-09 13:50:30 +0100

    subsonic - song list: added playlist filters to pagination

--- subsonic/dsp_songs_pager.php
+++ subsonic/dsp_songs_pager.php
@@ -1,7 +1,7 @@
 
 <?php if($pages > 1){ ?>
 	<ul class="pagination pagination-sm">
-		<li gd-page="<?=$page-1?>" <?php if($page == 1){ ?>class="disabled"<?php } ?>><a href="?action=<?= $action->getCode() ?>&amp;page=<?=$page-1?>&amp;perpage=<?=$perpage?>&amp;sort=<?=$sort?>&amp;sortorder=<?=$sortorder?>&amp;search=<?=$search?>">&laquo;</a></li>
+		<li gd-page="<?=$page-1?>" <?php if($page == 1){ ?>class="disabled"<?php } ?>><a href="?action=<?= $action->getCode() ?>&amp;page=<?=$page-1?>&amp;perpage=<?=$perpage?>&amp;sort=<?=$sort?>&amp;sortorder=<?=$sortorder?>&amp;search=<?=$search?>&amp;playlist=<?=$playlist?>&amp;playlistId=<?=$playlistId?>">&laquo;</a></li>
 		<?php if($show_first_dots){ ?><li <?php /*if($page == 1)*/{ ?>class="disabled"<?php } ?>><a href="#">&#8230;</a></li><?php } ?>
 		<?php 
 			for($i = $pages_start; $i <= $pages_end; $i++){
@@ -12,12 +12,12 @@
 				}
 				else {
 				?>
-					<li gd-page="<?=$i?>"><a href="?action=<?= $action->getCode() ?>&amp;page=<?=$i?>&amp;perpage=<?=$perpage?>&amp;sort=<?=$sort?>&amp;sortorder=<?=$sortorder?>&amp;search=<?=$search?>"><?=$i?></a></li>
+					<li gd-page="<?=$i?>"><a href="?action=<?= $action->getCode() ?>&amp;page=<?=$i?>&amp;perpage=<?=$perpage?>&amp;sort=<?=$sort?>&amp;sortorder=<?=$sortorder?>&amp;search=<?=$search?>&amp;playlist=<?=$playlist?>&amp;playlistId=<?=$playlistId?>"><?=$i?></a></li>
 				<?php
 				}
 			}
 		?>
 		<?php if($show_last_dots){ ?><li <?php /*if($page == $pages)*/{ ?>class="disabled"<?php } ?>><a href="#">&#8230;</a></li><?php } ?>
-		<li gd-page="<?=$page+1?>" <?php if($page == $pages){ ?>class="disabled"<?php } ?>><a href="?action=<?= $action->getCode() ?>&amp;page=<?=$page+1?>&amp;perpage=<?=$perpage?>&amp;sort=<?=$sort?>&amp;sortorder=<?=$sortorder?>&amp;search=<?=$search?>">&raquo;</a></li>
+		<li gd-page="<?=$page+1?>" <?php if($page == $pages){ ?>class="disabled"<?php } ?>><a href="?action=<?= $action->getCode() ?>&amp;page=<?=$page+1?>&amp;perpage=<?=$perpage?>&amp;sort=<?=$sort?>&amp;sortorder=<?=$sortorder?>&amp;search=<?=$search?>&amp;playlist=<?=$playlist?>&amp;playlistId=<?=$playlistId?>">&raquo;</a></li>
 	</ul>
 <?php } ?>
