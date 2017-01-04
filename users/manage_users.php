94ced6c3c64e271484df23aaf2f898250734349d
Wim <github@wikke.net>
2017-01-04 11:06:57 +0100

    users: schedule new users to fill in salt/password

---
+++ users/manage_users.php
@@ -0,0 +1,70 @@
+<?php
+set_time_limit(0);
+
+require dirname(__FILE__).'/../_core/appinit.php';
+
+require 'connection.php';
+require 'functions.php';
+
+
+if(!$task->getIsRunning())
+{
+	$task->setIsRunning(true);
+	
+	
+	// Set "salt" field for new users
+	$qry_users_new = mysqli_query($conn, "
+		select
+			id_user,
+			username
+		from t_user
+		where
+			ifnull(salt,'') = ''
+		");
+		
+	while($user = mysqli_fetch_array($qry_users_new)){
+		mysqli_query($conn, "
+			update t_user
+			set
+				salt = '" . hash('sha512', uniqid(openssl_random_pseudo_bytes(16), TRUE)) . "'
+			where
+				id_user = " . $user['id_user'] . "
+				and ifnull(salt,'') = ''
+			");
+		echo 'Salt for user "' . $user['username'] . '" set';
+	}
+	
+	
+	// Set "password" field for new users
+	$qry_users_new = mysqli_query($conn, "
+		select
+			id_user,
+			username,
+			salt
+		from t_user
+		where
+			ifnull(password,'') = ''
+			and ifnull(salt,'') <> ''
+			and ifnull(new_password,'') <> ''
+		");
+		
+	while($user = mysqli_fetch_array($qry_users_new)){
+		$password = $user['new_password'];
+		mysqli_query($conn, "
+			update t_user
+			set
+				password = '" . hash('sha512', $password . $user['salt']) . "',
+				new_password = ''
+			where
+				id_user = " . $user['id_user'] . "
+				and ifnull(password,'') = ''
+			");
+		echo 'Password for user "' . $user['username'] . '" set to "' . $password . '"';
+	}
+	
+	
+	
+	$task->setIsRunning(false);
+}
+
+?>
