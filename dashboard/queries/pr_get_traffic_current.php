901f3da519e7fbbf0e22a318781afa04953f1cd8
Wim <github@wikke.net>
2017-01-19 15:43:31 +0100

    dfsgh

--- dashboard/queries/pr_get_traffic_current.php
+++ dashboard/queries/pr_get_traffic_current.php
@@ -2,11 +2,11 @@
 
 $qry_timings_max = mysqli_query($conn, "
 	select
-		max(DATE_FORMAT(r.date, '%Y%m%d%H%i')) as latest,
-		max(DATE_FORMAT(r.date, '%H:%i')) as latest_formatted,
+		max(DATE_FORMAT(r.date, '%Y%m%d %H:%i')) as latest
 	from reistijden r
 	");
 $timings_max = mysqli_fetch_array($qry_timings_max);
+$timings_max_latest = $timings_max['latest'];
 
 $qry_timings = mysqli_query($conn, "
 	
@@ -19,7 +19,7 @@ $qry_timings = mysqli_query($conn, "
 		
 	from knooppunten k
 		left join reistijden r on r.knooppuntId = k.id
-			and DATE_FORMAT(r.date, '%Y%m%d%H%i') = '" . $timings_max['latest'] . "'
+			and DATE_FORMAT(r.date, '%Y%m%d %H:%i') = '" . $timings_max['latest'] . "'
 	
 	order by
 		r.date desc,
