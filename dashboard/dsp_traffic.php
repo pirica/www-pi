d6376c744ffffd5eca048d9d032dce612890d84f
Wim <github@wikke.net>
2017-01-19 15:57:32 +0100

    dashboard - traffi: colors + timings layout

--- dashboard/dsp_traffic.php
+++ dashboard/dsp_traffic.php
@@ -1,8 +1,4 @@
 
-mysqli_fetch_array($qry_timings_max);
-
-$qry_timings
-
 
 <h1>Traffic</h1>
 
@@ -12,11 +8,11 @@ $qry_timings
 		<div class="timings" style="position: relative;">
 			<img src="images/ring_antw.jpg" />
 			
-			<div style="position: absolute; left: 76%; top: 50%;">	<span><?= $timings['ao-az'] ?></span>	</div>
-			<div style="position: absolute; left: 67%; top: 50%;">	<span><?= $timings['az-ac'] ?></span>	</div>
+			<div class="to" style="position: absolute; left: 63%; top: 64%;">	<span><?= $timings['ao-az'] ?></span>	</div>
+			<div class="to" style="position: absolute; left: 53%; top: 73%;">	<span><?= $timings['az-ac'] ?></span>	</div>
 
-			<div style="position: absolute; left: 12%; top: 63%;">	<span><?= $timings['ac-az'] ?></span>	</div>
-			<div style="position: absolute; left: 22%; top: 63%;">	<span><?= $timings['az-ao'] ?></span>	</div>
+			<div class="from" style="position: absolute; left: 53%; top: 81%;">	<span><?= $timings['ac-az'] ?></span>	</div>
+			<div class="from" style="position: absolute; left: 66%; top: 69%;">	<span><?= $timings['az-ao'] ?></span>	</div>
 			
 		</div>
 	</div>
@@ -25,23 +21,23 @@ $qry_timings
 		<div class="timings" style="position: relative;">
 			<img src="images/e313.jpg" />
 			
-			<div style="position: absolute; left: 76%; top: 50%;">	<span><?= $timings['go-gw'] ?></span>	</div>
-			<div style="position: absolute; left: 67%; top: 50%;">	<span><?= $timings['gw-ho'] ?></span>	</div>
-			<div style="position: absolute; left: 58%; top: 50%;">	<span><?= $timings['ho-hi'] ?></span>	</div>
-			<div style="position: absolute; left: 49%; top: 50%;">	<span><?= $timings['hi-hw'] ?></span>	</div>
-			<div style="position: absolute; left: 40%; top: 50%;">	<span><?= $timings['hw-m'] ?></span>	</div>
-			<div style="position: absolute; left: 31%; top: 50%;">	<span><?= $timings['m-r'] ?></span>	</div>
-			<div style="position: absolute; left: 22%; top: 50%;">	<span><?= $timings['r-w'] ?></span>	</div>
-			<div style="position: absolute; left: 12%; top: 50%;">	<span><?= $timings['w-ao'] ?></span>	</div>
+			<div class="to" style="position: absolute; left: 76%; top: 50%;">	<span><?= $timings['go-gw'] ?></span>	</div>
+			<div class="to" style="position: absolute; left: 67%; top: 50%;">	<span><?= $timings['gw-ho'] ?></span>	</div>
+			<div class="to" style="position: absolute; left: 58%; top: 50%;">	<span><?= $timings['ho-hi'] ?></span>	</div>
+			<div class="to" style="position: absolute; left: 49%; top: 50%;">	<span><?= $timings['hi-hw'] ?></span>	</div>
+			<div class="to" style="position: absolute; left: 40%; top: 50%;">	<span><?= $timings['hw-m'] ?></span>	</div>
+			<div class="to" style="position: absolute; left: 31%; top: 50%;">	<span><?= $timings['m-r'] ?></span>	</div>
+			<div class="to" style="position: absolute; left: 22%; top: 50%;">	<span><?= $timings['r-w'] ?></span>	</div>
+			<div class="to" style="position: absolute; left: 12%; top: 50%;">	<span><?= $timings['w-ao'] ?></span>	</div>
 
-			<div style="position: absolute; left: 12%; top: 63%;">	<span><?= $timings['ao-w'] ?></span>	</div>
-			<div style="position: absolute; left: 22%; top: 63%;">	<span><?= $timings['w-r'] ?></span>	</div>
-			<div style="position: absolute; left: 31%; top: 63%;">	<span><?= $timings['r-m'] ?></span>	</div>
-			<div style="position: absolute; left: 40%; top: 63%;">	<span><?= $timings['m-hw'] ?></span>	</div>
-			<div style="position: absolute; left: 49%; top: 63%;">	<span><?= $timings['hw-hi'] ?></span>	</div>
-			<div style="position: absolute; left: 58%; top: 63%;">	<span><?= $timings['hi-ho'] ?></span>	</div>
-			<div style="position: absolute; left: 67%; top: 63%;">	<span><?= $timings['ho-gw'] ?></span>	</div>
-			<div style="position: absolute; left: 76%; top: 63%;">	<span><?= $timings['gw-go'] ?></span>	</div>
+			<div class="from" style="position: absolute; left: 12%; top: 63%;">	<span><?= $timings['ao-w'] ?></span>	</div>
+			<div class="from" style="position: absolute; left: 22%; top: 63%;">	<span><?= $timings['w-r'] ?></span>	</div>
+			<div class="from" style="position: absolute; left: 31%; top: 63%;">	<span><?= $timings['r-m'] ?></span>	</div>
+			<div class="from" style="position: absolute; left: 40%; top: 63%;">	<span><?= $timings['m-hw'] ?></span>	</div>
+			<div class="from" style="position: absolute; left: 49%; top: 63%;">	<span><?= $timings['hw-hi'] ?></span>	</div>
+			<div class="from" style="position: absolute; left: 58%; top: 63%;">	<span><?= $timings['hi-ho'] ?></span>	</div>
+			<div class="from" style="position: absolute; left: 67%; top: 63%;">	<span><?= $timings['ho-gw'] ?></span>	</div>
+			<div class="from" style="position: absolute; left: 76%; top: 63%;">	<span><?= $timings['gw-go'] ?></span>	</div>
 		</div>
 	</div>
 	
