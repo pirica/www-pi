62a0130fc85875e0f36e7edcc4e609c30275176e
Wim <github@wikke.net>
2017-04-27 16:02:07 +0200

    lego - indexes: try catch

--- lego/jobs/check_indexes.php
+++ lego/jobs/check_indexes.php
@@ -13,51 +13,57 @@ if(!$task->getIsRunning())
 	
 	if($settings->val('reindex_all', 0) == 1)
 	{
-		
-		$str = file_get_contents('http://lego.brickinstructions.com/en/showallyears');
-
-		$doc = phpQuery::newDocumentHTML($str);
-
-		$links = $doc->find('.yearTable a');
-		foreach ($links as $link)
-		{
-			mysqli_query($conn, "
-				insert into indexYear
-				(
-					year
-				)
-				select
-					year
-				from (select '" . mysqli_real_escape_string($conn, pq($link)->html()) . "' as year) tmp
-				where not exists(
-					select * from indexYear where year = '" . mysqli_real_escape_string($conn, pq($link)->html()) . "'
-				)
-			");
-			
-			mysqli_query($conn, "
-				insert into indexByYear
-				(
-					year,
-					page
-				)
-				select
-					year,
-					page
-				from (select 
-					'" . mysqli_real_escape_string($conn, pq($link)->html()) . "' as year,
-					1 as page
-				) tmp
-				where not exists(
-					select * from indexByYear
-					where year = '" . mysqli_real_escape_string($conn, pq($link)->html()) . "'
-						and page = 1
-				)
-			");
+		$str = false;
+		try {
+			$str = file_get_contents('http://lego.brickinstructions.com/en/showallyears');
 		}
+		catch (Exception $e) {}
 		
-		mysqli_query($conn, "update indexYear set indexed = 0");
-		mysqli_query($conn, "update indexByYear set indexed = 0");
-		mysqli_query($conn, "update indexByPage set indexed = 0");
+		if($str !== false)
+		{
+			$doc = phpQuery::newDocumentHTML($str);
+
+			$links = $doc->find('.yearTable a');
+			foreach ($links as $link)
+			{
+				mysqli_query($conn, "
+					insert into indexYear
+					(
+						year
+					)
+					select
+						year
+					from (select '" . mysqli_real_escape_string($conn, pq($link)->html()) . "' as year) tmp
+					where not exists(
+						select * from indexYear where year = '" . mysqli_real_escape_string($conn, pq($link)->html()) . "'
+					)
+				");
+				
+				mysqli_query($conn, "
+					insert into indexByYear
+					(
+						year,
+						page
+					)
+					select
+						year,
+						page
+					from (select 
+						'" . mysqli_real_escape_string($conn, pq($link)->html()) . "' as year,
+						1 as page
+					) tmp
+					where not exists(
+						select * from indexByYear
+						where year = '" . mysqli_real_escape_string($conn, pq($link)->html()) . "'
+							and page = 1
+					)
+				");
+			}
+			
+			mysqli_query($conn, "update indexYear set indexed = 0");
+			mysqli_query($conn, "update indexByYear set indexed = 0");
+			mysqli_query($conn, "update indexByPage set indexed = 0");
+		}
 	}
 	
 	
@@ -71,36 +77,42 @@ if(!$task->getIsRunning())
 		");
 
 	while ($years = mysqli_fetch_array($qry)) {
-		
-		$str = file_get_contents('http://lego.brickinstructions.com/search/year/' . $years['year']);
-
-		$doc = phpQuery::newDocumentHTML($str);
-
-		$links = $doc->find('.paginateGroup a');
-		foreach ($links as $link)
-		{
-			mysqli_query($conn, "
-				insert into indexByYear
-				(
-					year,
-					page
-				)
-				select
-					year,
-					page
-				from (select 
-					'" . mysqli_real_escape_string($conn, $years['year']) . "' as year,
-					'" . mysqli_real_escape_string($conn, pq($link)->html()) . "' as page
-				) tmp
-				where not exists(
-					select * from indexByYear
-					where year = '" . mysqli_real_escape_string($conn, $years['year']) . "'
-						and page = '" . mysqli_real_escape_string($conn, pq($link)->html()) . "'
-				)
-			");
+		$str = false;
+		try {
+			$str = file_get_contents('http://lego.brickinstructions.com/search/year/' . $years['year']);
 		}
+		catch (Exception $e) {}
 		
-		mysqli_query($conn, "update indexYear set indexed = 1 where year = '" . mysqli_real_escape_string($conn, $years['year']) . "' ");
+		if($str !== false)
+		{
+			$doc = phpQuery::newDocumentHTML($str);
+
+			$links = $doc->find('.paginateGroup a');
+			foreach ($links as $link)
+			{
+				mysqli_query($conn, "
+					insert into indexByYear
+					(
+						year,
+						page
+					)
+					select
+						year,
+						page
+					from (select 
+						'" . mysqli_real_escape_string($conn, $years['year']) . "' as year,
+						'" . mysqli_real_escape_string($conn, pq($link)->html()) . "' as page
+					) tmp
+					where not exists(
+						select * from indexByYear
+						where year = '" . mysqli_real_escape_string($conn, $years['year']) . "'
+							and page = '" . mysqli_real_escape_string($conn, pq($link)->html()) . "'
+					)
+				");
+			}
+			
+			mysqli_query($conn, "update indexYear set indexed = 1 where year = '" . mysqli_real_escape_string($conn, $years['year']) . "' ");
+		}
 	}
 	
 	
@@ -116,58 +128,64 @@ if(!$task->getIsRunning())
 		");
 
 	while ($pages = mysqli_fetch_array($qry)) {
+		$str = false;
+		try {
+			$str = file_get_contents('http://lego.brickinstructions.com/search/year/' . $pages['year'] . ($pages['page'] == 1 ? '' : '/' . $pages['page']));
+		}
+		catch (Exception $e) {}
 		
-		$str = file_get_contents('http://lego.brickinstructions.com/search/year/' . $pages['year'] . ($pages['page'] == 1 ? '' : '/' . $pages['page']));
-
-		$doc = phpQuery::newDocumentHTML($str);
-
-		$sets = $doc->find('.setBox a');
-		foreach ($sets as $set)
+		if($str !== false)
 		{
-			if(stripos(pq($set)->html(), '<img') === false)
+			$doc = phpQuery::newDocumentHTML($str);
+
+			$sets = $doc->find('.setBox a');
+			foreach ($sets as $set)
 			{
-				$linkparts = explode('/', pq($set)->attr('href'));
-				$setnr = '';
-				$setcode = '';
-				if(count($linkparts) > 3)
+				if(stripos(pq($set)->html(), '<img') === false)
 				{
-					$setnr = $linkparts[count($linkparts) - 2];
-					$setcode = $linkparts[count($linkparts) - 1];
-					mysqli_query($conn, "
-						insert into indexByPage
-						(
-							`year`,
-							`set`,
-							code,
-							name
-						)
-						select
-							`year`,
-							`set`,
-							code,
-							name
-						from (select 
-							'" . mysqli_real_escape_string($conn, $pages['year']) . "' as `year`,
-							'" . mysqli_real_escape_string($conn, $setnr) . "' as `set`,
-							'" . mysqli_real_escape_string($conn, $setcode) . "' as code,
-							'" . mysqli_real_escape_string($conn, pq($set)->html()) . "' as name
-						) tmp
-						where not exists(
-							select * from indexByPage
-							where `set` = '" . mysqli_real_escape_string($conn, $setnr) . "'
-						)
-					");
+					$linkparts = explode('/', pq($set)->attr('href'));
+					$setnr = '';
+					$setcode = '';
+					if(count($linkparts) > 3)
+					{
+						$setnr = $linkparts[count($linkparts) - 2];
+						$setcode = $linkparts[count($linkparts) - 1];
+						mysqli_query($conn, "
+							insert into indexByPage
+							(
+								`year`,
+								`set`,
+								code,
+								name
+							)
+							select
+								`year`,
+								`set`,
+								code,
+								name
+							from (select 
+								'" . mysqli_real_escape_string($conn, $pages['year']) . "' as `year`,
+								'" . mysqli_real_escape_string($conn, $setnr) . "' as `set`,
+								'" . mysqli_real_escape_string($conn, $setcode) . "' as code,
+								'" . mysqli_real_escape_string($conn, pq($set)->html()) . "' as name
+							) tmp
+							where not exists(
+								select * from indexByPage
+								where `set` = '" . mysqli_real_escape_string($conn, $setnr) . "'
+							)
+						");
+					}
 				}
 			}
-		}
-		
-		mysqli_query($conn, "
-			update indexByYear 
-			set indexed = 1 
-			where year = '" . mysqli_real_escape_string($conn, $pages['year']) . "' 
-			and page = '" . mysqli_real_escape_string($conn, $pages['page']) . "' 
 			
-			");
+			mysqli_query($conn, "
+				update indexByYear 
+				set indexed = 1 
+				where year = '" . mysqli_real_escape_string($conn, $pages['year']) . "' 
+				and page = '" . mysqli_real_escape_string($conn, $pages['page']) . "' 
+				
+				");
+		}
 	}
 	
 	
@@ -184,70 +202,75 @@ if(!$task->getIsRunning())
 		");
 
 	while ($pages = mysqli_fetch_array($qry)) {
-		
-		$str = file_get_contents('http://lego.brickinstructions.com/en/lego_instructions/set/' . $pages['set'] . '/' . $pages['code']);
-
-		$doc = phpQuery::newDocumentHTML($str);
-
-		$images = $doc->find('#instructionContainer a');
-		foreach ($images as $image)
-		{
-			$linkparts = explode('/', pq($image)->attr('href'));
-			$filename = '';
-			if(count($linkparts) > 3)
-			{
-				$filename = $linkparts[count($linkparts) - 1];
-			}
-			mysqli_query($conn, "
-				insert into indexPage
-				(
-					`set`,
-					image_url,
-					filename
-				)
-				select
-					`set`,
-					image_url,
-					filename
-				from (select 
-					'" . mysqli_real_escape_string($conn, $pages['set']) . "' as `set`,
-					'" . mysqli_real_escape_string($conn, pq($image)->attr('href')) . "' as image_url,
-					'" . mysqli_real_escape_string($conn, $filename) . "' as filename
-				) tmp
-				where not exists(
-					select * from indexPage
-					where `set` = '" . mysqli_real_escape_string($conn, $pages['set']) . "'
-						and image_url = '" . mysqli_real_escape_string($conn, pq($image)->attr('href')) . "'
-				)
-			");
-			
+		$str = false;
+		try {
+			$str = file_get_contents('http://lego.brickinstructions.com/en/lego_instructions/set/' . $pages['set'] . '/' . $pages['code']);
 		}
+		catch (Exception $e) {}
 		
-		mysqli_query($conn, "
-			update indexByPage 
-			set indexed = 1 
-			where `set` = '" . mysqli_real_escape_string($conn, $pages['set']) . "' 
+		if($str !== false)
+		{
+			$doc = phpQuery::newDocumentHTML($str);
+
+			$images = $doc->find('#instructionContainer a');
+			foreach ($images as $image)
+			{
+				$linkparts = explode('/', pq($image)->attr('href'));
+				$filename = '';
+				if(count($linkparts) > 3)
+				{
+					$filename = $linkparts[count($linkparts) - 1];
+				}
+				mysqli_query($conn, "
+					insert into indexPage
+					(
+						`set`,
+						image_url,
+						filename
+					)
+					select
+						`set`,
+						image_url,
+						filename
+					from (select 
+						'" . mysqli_real_escape_string($conn, $pages['set']) . "' as `set`,
+						'" . mysqli_real_escape_string($conn, pq($image)->attr('href')) . "' as image_url,
+						'" . mysqli_real_escape_string($conn, $filename) . "' as filename
+					) tmp
+					where not exists(
+						select * from indexPage
+						where `set` = '" . mysqli_real_escape_string($conn, $pages['set']) . "'
+							and image_url = '" . mysqli_real_escape_string($conn, pq($image)->attr('href')) . "'
+					)
+				");
+			}
 			
-			");
+			mysqli_query($conn, "
+				update indexByPage 
+				set indexed = 1 
+				where `set` = '" . mysqli_real_escape_string($conn, $pages['set']) . "' 
+				
+				");
+		}
 	}
 	
 	
 	
 	$id_grab = $settings->val('manual_downloads_id_grab',0);
-	$dir = $settings->val('manuals_directory','');
+	$manuals_directory = $settings->val('manuals_directory','');
 	
-	if($manuals_directory != '')
+	if($manuals_directory != '' && $id_grab > 0)
 	{
 		// images
 		mysqli_query($conn, "
 			
-			insert into t_grab_file (id_grab, full_url, full_path) 
+			insert into grabber.t_grab_file (id_grab, full_url, full_path) 
 			select 
 				" . $id_grab . ", 
 				concat('http://lego.brickinstructions.com', p.image_url),  
 				concat('" . $manuals_directory . "', p.`set`, '/', p.filename)
 			from indexPage p
-				left join t_grab_file gf on gf.full_url = concat('http://lego.brickinstructions.com', p.image_url) and gf.id_grab = " . $id_grab . "
+				left join grabber.t_grab_file gf on gf.full_url = concat('http://lego.brickinstructions.com', p.image_url) and gf.id_grab = " . $id_grab . "
 			where  
 				p.image_url like '/%'
 				and gf.id_grab_file is null 
@@ -257,13 +280,13 @@ if(!$task->getIsRunning())
 		// PDF's
 		mysqli_query($conn, "
 			
-			insert into t_grab_file (id_grab, full_url, full_path) 
+			insert into grabber.t_grab_file (id_grab, full_url, full_path) 
 			select 
 				" . $id_grab . ", 
 				p.image_url,  
 				concat('" . $manuals_directory . "', p.`set`, '/', p.filename)
 			from indexPage p
-				left join t_grab_file gf on gf.full_url = p.image_url and gf.id_grab = " . $id_grab . "
+				left join grabber.t_grab_file gf on gf.full_url = p.image_url and gf.id_grab = " . $id_grab . "
 			where  
 				p.image_url not like '/%'
 				and p.filename like '%.pdf'
