739ca56d837c4233a548fc06da771acb6a9c8605
Wim <github@wikke.net>
2017-05-08 11:32:19 +0200

    zeqezrert

--- lego/queries/qry_sets.php
+++ lego/queries/qry_sets.php
@@ -4,9 +4,9 @@ $qry_sets = mysqli_query($conn, "
 
 	select
 		substring_index(s.set_num,'-',1) as set_num,
-		s.name
-		s.year
-		s.theme_id
+		s.name,
+		s.year,
+		s.theme_id,
 		s.num_parts
 		
 	from sets s
