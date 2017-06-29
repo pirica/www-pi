d627d50db40e44997c3558e1b36c24f7899236db
Wim <github@wikke.net>
2017-06-29 15:47:33 +0200

    camera - build gifs: rebuild when movie is updated

--- camera/jobs/build_gifs.php
+++ camera/jobs/build_gifs.php
@@ -51,11 +51,19 @@ if(!$task->getIsRunning())
 		$pallete = '/dev/shm/palette' . str_replace('.avi', '.png', $camera_log['name']);
 		
 		if(strtolower($extension) == '.avi'){
-			if(!file_exists($gifname)){
+			if(
+				!file_exists($gifname)
+				||
+				(filemtime($gifname) < filemtime($main_dir . $camera_log['date'] . '/' . $camera_log['name'])
+			){
 				
-				shell_exec('ffmpeg -y -i ' . $main_dir . $camera_log['date'] . '/' . $camera_log['name'] . ' -vf fps=5,scale='.$thumbWidth.':-1:flags=lanczos,palettegen '.$pallete);
-				shell_exec('ffmpeg -i ' . $main_dir . $camera_log['date'] . '/' . $camera_log['name'] . ' -i '.$pallete.' -filter_complex "fps=5,scale='.$thumbWidth.':-1:flags=lanczos[x];[x][1:v]paletteuse" ' . $gifname);
+				shell_exec('ffmpeg -y -i ' . $main_dir . $camera_log['date'] . '/' . $camera_log['name'] . ' -vf fps=2,scale='.$thumbWidth.':-1:flags=lanczos,palettegen '.$pallete);
+				shell_exec('ffmpeg -i ' . $main_dir . $camera_log['date'] . '/' . $camera_log['name'] . ' -i '.$pallete.' -filter_complex "fps=2,scale='.$thumbWidth.':-1:flags=lanczos[x];[x][1:v]paletteuse" ' . $gifname);
 				
+				//gifsicle -i /var/docs/motion/20170629/20170629_133203_backdoor_09.gif -O3 -o /var/docs/motion/20170629/20170629_133203_backdoor_09b.gif
+				//gifsicle -i /var/docs/motion/20170629/20170629_133203_backdoor_09.gif -O3 --colors 256 -o /var/docs/motion/20170629/20170629_133203_backdoor_09b.gif
+				
+				//ffmpeg -i /var/docs/motion/20170629/20170629_133203_backdoor_09.avi -r 10 -f image2pipe -vcodec ppm - | convert -delay 10 - gif:- | convert -layers Optimize - /var/docs/motion/20170629/20170629_133203_backdoor_09d.gif
 				
 			}
 		}
